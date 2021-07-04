32971395b97f6569cba30887a06df310530b5624
Leandro Santos <ljsrsantos@gmail.com>
2021-07-04 00:40:54 -0300

    typeorm and migrations config

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,9 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'reflect-metadata';
+import '@shared/typeorm';
 import cors from 'cors';
 import routes from './routes';
+import AppError from '@shared/errors/AppError';
 
 const app = express();
 
@@ -9,6 +12,22 @@ app.use(express.json());
 
 app.use(routes);
 
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'sorry internal server error',
+    });
+  },
+);
+
 app.listen(3333, () => {
-  console.log('teste leandro');
+  console.log('****** APP UP ******');
 });
