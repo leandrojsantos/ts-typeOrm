da5b087b2f5f58f4f74c89a55f5f4527ff1d79c8
leandrojsantos <ljsrsantos@gmail.com>
2022-07-13 22:35:02 -0300

    code clear

--- src/modules/products/routes/products.routes.ts
+++ src/modules/products/routes/products.routes.ts
@@ -8,34 +8,35 @@ const productsController = new ProductsController();
 productsRouter.get('/', productsController.index);
 
 productsRouter.post('/', celebrate({[Segments.BODY]: {
-      name: Joi.string().required(),
-      price: Joi.number().required().precision(2),
-      quantity: Joi.number().required(),
+    name: Joi.string().required(),
+    price: Joi.number().required().precision(2),
+    quantity: Joi.number().required(),
     },
   }),
   productsController.create,
 );
 
 productsRouter.get('/:id', celebrate({[Segments.PARAMS]: {
-      id: Joi.string().uuid().required(),
+    id: Joi.string().uuid().required(),
     },
   }),
   productsController.show,
 );
 
 productsRouter.put('/:id', celebrate({[Segments.PARAMS]: {
-      id: Joi.string().uuid().required(),
-    }, [Segments.BODY]: {
-      name: Joi.string().required(),
-      price: Joi.number().required().precision(2),
-      quantity: Joi.number().required(),
+    id: Joi.string().uuid().required(),
+    },
+    [Segments.BODY]: {
+    name: Joi.string().required(),
+    price: Joi.number().required().precision(2),
+    quantity: Joi.number().required(),
     },
   }),
   productsController.update,
 );
 
 productsRouter.delete('/:id', celebrate({[Segments.PARAMS]: {
-      id: Joi.string().uuid().required(),
+    id: Joi.string().uuid().required(),
     },
   }),
   productsController.delete,
